b248b0fb5067e34a46eaa6220d82917617ca69d8
thdtjsdn <thdtjsdn@gmail.com>
2020-02-05 23:03:49 +0900

    Update _____list에서 복사해올 파일 복사해 오기.bat

--- data/_____list에서 복사해올 파일 복사해 오기.bat
+++ data/_____list에서 복사해올 파일 복사해 오기.bat
@@ -2,3 +2,21 @@ chcp 65001
 
 copy list\"*개 모음 - *.json" .
 copy list\"*개 이하 모음 - *.json" .
+
+copy "list\2일 연속 하락 후 - 1일 - 03*.json" .
+copy "list\3일 연속 하락 후 - 1일 - 03*.json" .
+copy "list\4일 연속 하락 후 - 1일 - 03*.json" .
+copy "list\5일 연속 하락 후 - 1일 - 03*.json" .
+copy "list\6일 연속 하락 후 - 1일 - 03*.json" .
+copy "list\7일 연속 하락 후 - 1일 - 03*.json" .
+copy "list\8일 연속 하락 후 - 1일 - 03*.json" .
+copy "list\9일 연속 하락 후 - 1일 - 03*.json" .
+
+copy "list\2일 연속 하락 후 - 1일 - 05*.json" .
+copy "list\3일 연속 하락 후 - 1일 - 05*.json" .
+copy "list\4일 연속 하락 후 - 1일 - 05*.json" .
+copy "list\5일 연속 하락 후 - 1일 - 05*.json" .
+copy "list\6일 연속 하락 후 - 1일 - 05*.json" .
+copy "list\7일 연속 하락 후 - 1일 - 05*.json" .
+copy "list\8일 연속 하락 후 - 1일 - 05*.json" .
+copy "list\9일 연속 하락 후 - 1일 - 05*.json" .
