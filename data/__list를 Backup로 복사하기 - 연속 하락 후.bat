e70674f31a694b7198629692056cb99a46e8b23a
thdtjsdn <thdtjsdn@gmail.com>
2020-02-20 20:17:48 +0900

    N

---
+++ data/__list를 Backup로 복사하기 - 연속 하락 후.bat
@@ -0,0 +1,6 @@
+chcp 65001
+
+copy "*일 연속 하락 후 - 1일 - 03*.json" Backup\
+copy "*일 연속 하락 후 - 1일 - 05*.json" Backup\
+
+:cmd /k
---
+++ data/__list에서 복사하기.bat
@@ -0,0 +1,15 @@
+chcp 65001
+
+copy "list\*개 이하 모음 - *.json" .
+
+::
+
+copy "list\*일 연속 상승 후 - 1일 - -03*.json" .
+copy "list\*일 연속 상승 후 - 1일 - -05*.json" .
+
+::
+
+copy "list\*일 연속 하락 후 - 1일 - 03*.json" .
+copy "list\*일 연속 하락 후 - 1일 - 05*.json" .
+
+cmd /k
