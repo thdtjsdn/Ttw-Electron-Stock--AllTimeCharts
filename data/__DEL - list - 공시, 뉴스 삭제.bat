5d348bd286bc66c4d9e25a733a06173594dfb2ca
thdtjsdn <thdtjsdn@gmail.com>
2020-05-12 00:59:54 +0900

    N

---
+++ data/__DEL - list - 공시, 뉴스 삭제.bat
@@ -0,0 +1,6 @@
+chcp 65001
+
+del "list - 공시 - *"
+del "list - 뉴스 - *"
+
+cmd /k
---
+++ data/__DEL - list - 전체 병합을 위한 삭제.bat
@@ -0,0 +1,19 @@
+chcp 65001
+
+cd Backup
+
+del "list - ttw_관심 - *"
+del "list - ttw_관심_*"
+del "list - ttw_우량 - *"
+del "list - 관심종목(*"
+del "list - 공시 - *"
+del "list - 뉴스 - *"
+
+del "* - 거래 금액 순위(시작가) 200개*"
+
+del "* - 공시 - 호재 - *"
+del "* - 뉴스 - 호재 - *"
+
+del "* - 등락률 순위*"
+
+:cmd /k
