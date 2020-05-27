dc6fca17ce2277e19c65e29bae1d017a8ae0ebb6
thdtjsdn <thdtjsdn@gmail.com>
2020-05-28 02:11:58 +0900

    N, U

--- data/__COPY - 방송 종목 목록 - 9%미만.bat
+++ data/__COPY - 방송 종목 목록 - 9%미만.bat
@@ -1,15 +1,15 @@
 chcp 65001
 
-copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200521\list - 음성 기록 통계 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
+copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
 
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200521\list - 음성 기록 통계 - 0.5 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200521\list - 음성 기록 통계 - 0.7 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200521\list - 음성 기록 통계 - -0.7 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200521\list - 음성 기록 통계 - 1 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200521\list - 음성 기록 통계 - -1 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200521\list - 음성 기록 통계 - 2 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200521\list - 음성 기록 통계 - -2 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200521\list - 음성 기록 통계 - 3 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200521\list - 음성 기록 통계 - -3 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\'
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - 0.5 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - 0.7 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - -0.7 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - 1 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - -1 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - 2 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - -2 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - 3 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\"
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - -3 - 등락(9*.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\'
 
 :cmd /k
