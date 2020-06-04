7a0cb0614dbf93452eab457c0724c3c2eb107559
thdtjsdn <thdtjsdn@gmail.com>
2020-06-05 00:00:32 +0900

    Update __COPY - 방송 종목 목록.bat

--- data/__COPY - 방송 종목 목록.bat
+++ data/__COPY - 방송 종목 목록.bat
@@ -1,15 +1,15 @@
 chcp 65001
 
-copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
+copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200604\list - 음성 기록 통계.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
 
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - 0.5.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - 0.7.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - -0.7.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - 1.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - -1.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - 2.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - -2.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - 3.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
-:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200527\list - 음성 기록 통계 - -3.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200604\list - 음성 기록 통계 - 0.5.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200604\list - 음성 기록 통계 - 0.7.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200604\list - 음성 기록 통계 - -0.7.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200604\list - 음성 기록 통계 - 1.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200604\list - 음성 기록 통계 - -1.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200604\list - 음성 기록 통계 - 2.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200604\list - 음성 기록 통계 - -2.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200604\list - 음성 기록 통계 - 3.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
+:copy "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--SelectedChartList-RT--Data - 방송\20200604\list - 음성 기록 통계 - -3.json" "E:\GitHub_Ttw--Stock\Ttw-Electron-Stock--AllTimeCharts\data\Backup\
 
 :cmd /k
